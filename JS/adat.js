53329e4b508615b35790e528569c51ddf84469f9
BeldyAdam <115634783+BeldyAdam@users.noreply.github.com>
2023-04-19 09:32:27 +0200

    Add files via upload

--- JS/adat.js
+++ JS/adat.js
@@ -1,13 +1,13 @@
 export const ADATLISTA = [
   { nev: "Beppo", kor: 12, fajta: "Agár" ,kep: "KEPEK/dog.jpg"},
-  { nev: "Da Vinci", kor: 5, fajta: "Spániel" ,kep:"KEPEK/dog.jpg" },
-  { nev: "Coco", kor: 8, fajta: "Masztiff",kep:"KEPEK/dog.jpg" },
-  { nev: "Bella", kor: 1, fajta: "Keverék" ,kep:"KEPEK/dog.jpg"},
-  { nev: "Zuri", kor: 9, fajta: "Bernáthegyi" ,kep:"KEPEK/dog.jpg"},
-  { nev: "Beppo", kor: 12, fajta: "Agár" ,kep: "KEPEK/dog.jpg"},
-  { nev: "Da Vinci", kor: 5, fajta: "Spániel" ,kep:"KEPEK/dog.jpg" },
-  { nev: "Coco", kor: 8, fajta: "Masztiff",kep:"KEPEK/dog.jpg" },
-  { nev: "Bella", kor: 1, fajta: "Keverék" ,kep:"KEPEK/dog.jpg"},
+  { nev: "Da Vinci", kor: 5, fajta: "Spániel" ,kep:"KEPEK/1.jpg" },
+  { nev: "Coco", kor: 8, fajta: "Masztiff",kep:"KEPEK/2.jpg" },
+  { nev: "Bella", kor: 1, fajta: "Keverék" ,kep:"KEPEK/3.jpg"},
+  { nev: "Zuri", kor: 9, fajta: "Bernáthegyi" ,kep:"KEPEK/4.jpg"},
+  { nev: "Beppo", kor: 12, fajta: "Agár" ,kep: "KEPEK/5.jpg"},
+  { nev: "Da Vinci", kor: 5, fajta: "Spániel" ,kep:"KEPEK/6.jpg" },
+  { nev: "Coco", kor: 8, fajta: "Masztiff",kep:"KEPEK/2.jpg" },
+  { nev: "Bella", kor: 1, fajta: "Keverék" ,kep:"KEPEK/5.jpg"},
   { nev: "Zuri", kor: 9, fajta: "Bernáthegyi" ,kep:"KEPEK/dog.jpg"}
 ];
 
--- JS/webshop.js
+++ JS/webshop.js
@@ -13,3 +13,55 @@ function init() {
   const ARTICLEELEM = $("article");
   ARTICLEELEM.html(listaRendezesPublic(ADATLISTA));
 }
+
+
+function kosar()
+{
+    let container = $("#publik");
+    for(let i =0;i<ADATLISTA.length;i++)
+    {
+        let row = `<div class="card m-1" id="gallery-images" style="width: 18rem;">
+        <img class="card-img-top" src="./images/teszt.jpg" alt="Card image cap">
+        <div class="card-body">
+        <h5 class="card-title">${ADATLISTA[i].nev}</h5>
+        <p class="card-text">Fajta:${ADATLISTA[i].fajta}</p>
+        <p class="card-text">Kor:${ADATLISTA[i].kor}</p>
+        <a href="#" class="btn btn-primary megvizsgal">Megvizsgál</a>
+        <button href="#" class="btn btn-primary" value="${i}" id="buy">Kosárba tesz</button>
+      </div>
+        </div>`;
+        container.append(row);
+     }
+}
+kosar();
+
+$(".btn").click('#buy',function(){
+    let index = $(this).val();
+
+    let newrecord = {nev:ADATLISTA[index].nev,fajta:ADATLISTA[index].fajta,kor:ADATLISTA[index].kor,lab:ADATLISTA[index].lab};
+    
+kosarbaRak(newrecord,index);
+});
+
+
+
+function kosarbaRak(newrecord,id)
+{
+    let container = document.getElementById("kosar");
+    let row = `<div class="card m-1" id="remove_${id}>
+        <img class="card-img-top" src="./images/teszt.jpg" alt="Card image cap">
+        <div class="card-body">
+        <h5 class="card-title">${nev}</h4>
+        <p class="card-text">Fajta:${fajta}</p>
+        <p class="card-text">Kor:${kor}</p>
+        <a class="btn btn-primary">Megvizsgál</a>
+        <button  class="btn btn-primary" value="${id}" id="delete">✖</button>
+      </div>
+        </div>`
+        container.innerHTML+=row;
+}
+
+$(document).on('click', '#delete', function() {
+    let index = $(this).val();
+    document.getElementById("remove_"+index).remove();
+  });
